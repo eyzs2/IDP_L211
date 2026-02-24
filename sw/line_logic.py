d1fe8df628ef7cdc6d400e62eb5940dae9962dd7
Ethan Sia <siayzethan@gmail.com>
2026-02-24 11:58:57 +0000

    added pseudocode for robot logic

---
+++ sw/line_logic.py
@@ -0,0 +1,15 @@
+from machine import Pin
+
+def onLine():
+    leftonLinePin = 26
+    rightonLinePin = 27
+    leftonLine = Pin(leftonLinePin, Pin.IN, Pin.PULL_DOWN)
+    leftonLine.irq(handler=offLine)
+    rightonLine = Pin(rightonLinePin, Pin.IN, Pin.PULL_DOWN)
+    rightonLine.irq(handler=offLine)
+
+def offLine():
+    pass
+
+
+    
