04989c913cb7ba83f379e160e84efa1a1faac6b4
em966 <em966@cam.ac.uk>
2026-03-04 12:20:56 +0000

    done

--- sw/start_box.py
+++ sw/start_box.py
@@ -6,7 +6,7 @@ RIGHT = 1
 def exit_start_box(line, motors, motorspeed=35, confirm_ms=120, timeout_ms=6000):
 
     start_time = ticks_ms()
-    white_detected_since = None
+    confirm_start = None
 
     while ticks_diff(ticks_ms(), start_time) < timeout_ms:
 
@@ -18,18 +18,29 @@ def exit_start_box(line, motors, motorspeed=35, confirm_ms=120, timeout_ms=6000)
         left_front_on_line = line.leftOn.value()
         right_front_on_line = line.rightOn.value()
 
+        print(f"LEFT={left_front_on_line} RIGHT={right_front_on_line}")
+
+
          # both must be true (white)
         if left_front_on_line == 1 and right_front_on_line == 1:
-            motors[LEFT].off()
-            motors[RIGHT].off()
-            sleep(0.05)
-            return True
+            if confirm_start is None:
+                print("Line detected, confirming...")
+                confirm_start = ticks_ms()
+            elif ticks_diff(ticks_ms(), confirm_start) > confirm_ms:
+                motors[LEFT].off()
+                motors[RIGHT].off()
+                print("Line detected, exiting start box...")
+                sleep(0.05)
+                return True
+        else:
+            confirm_start = None
 
         sleep(0.01)
 
     # Timeout safety
     motors[LEFT].off()
     motors[RIGHT].off()
+    print("Timeout: Failed to detect line within time limit.")
     return False
 
 
